4281cf6d542b0e881cae07b7cf1f12d0f3a3f943
Hanri <henry018@laposte.net>
2022-02-02 15:30:18 +0100

    replaced BLoC with Riverpod for State Management, simple countdown state

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:otp_generator/pages/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:otp_generator/ticker.dart';
-import 'package:otp_generator/timer_bloc/timer_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,11 +22,11 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider(
-        create: (context) => TimerBloc(ticker: Ticker()),
-        child: const HomeScreen(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
 
+final timerProvider = StateNotifierProvider<TimerNotifier, TimerModel>(
+  (ref) => TimerNotifier()
+);
--- lib/ticker.dart
+++ lib/ticker.dart
@@ -1,11 +1,46 @@
-class Ticker {
+import 'dart:async';
 
-  Stream<int> tick() {
-    return Stream.periodic(const Duration(seconds: 1), (_) => getRemainingTime()).take(30);
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TimerNotifier extends StateNotifier<TimerModel>{
+  TimerNotifier() : super(_initialState);
+
+  static final _initialState = TimerModel(Ticker.getRemainingTime());
+
+  final Ticker _ticker = Ticker();
+  StreamSubscription<int>? _tickerSubscription;
+
+  void start() {
+    _startTimer();
   }
 
+  void _startTimer() {
+    _tickerSubscription?.cancel();
+    _tickerSubscription =
+        _ticker.tick().listen((duration) {
+          state = TimerModel(duration);
+        });
+    state = TimerModel(Ticker.getRemainingTime());
+  }
+
+  @override
+  void dispose() {
+    _tickerSubscription?.cancel();
+    super.dispose();
+  }
+}
+
+class Ticker {
+  Stream<int> tick() {
+    return Stream.periodic(const Duration(seconds: 1), (_) => getRemainingTime());
+  }
   static int getRemainingTime() {
     return 30 - (DateTime.now().millisecondsSinceEpoch ~/ Duration.millisecondsPerSecond)%30;
   }
+}
 
+class TimerModel {
+  final int timeLeft;
+
+  TimerModel(this.timeLeft);
 }
